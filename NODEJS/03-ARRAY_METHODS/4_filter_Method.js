43d0e4a4ea8c30c93f73e1eb20048d7cd8211e4c
abhijeet raut <abhijeet00900@gmail.com>
2023-02-10 18:06:00 +0530

    Array Method - filter() Method

--- NODEJS/03-ARRAY_METHODS/4_filter_Method.js
+++ NODEJS/03-ARRAY_METHODS/4_filter_Method.js
@@ -15,7 +15,78 @@
        not visited.
 */
 
+//Filter using callbackFn
+function isGreaterThan10(value){
+    return value > 10 ;
+}
 
+const array_1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+const filteredArray_1 = array_1.filter(isGreaterThan10);
+console.log("array_1 : ",array_1);
+console.log("filteredArray_1 : ", filteredArray_1);
+
+
+//Filter invalid entries from JSON : 
+const array_2 = [
+    {id : 12},
+    {id : -4},
+    {id : 0},
+    {id : 5},
+    {id : 16.5},
+    {},
+    {id : null},
+    {id : NaN},
+    {id : "undefined" }
+];
+
+let invalidEntries = 0;
+
+function filterByID(item){
+if(Number.isFinite(item.id) && item.id !== 0){
+    return true;
+}
+invalidEntries++;
+return false;
+}
+
+const filteredArray_2 = array_2.filter(filterByID);
+
+console.log("filteredArray_2 : ",filteredArray_2);
+
+console.log("invalidItems : ",invalidEntries);
+
+
+//Searching in an array 
+const fruits = ["apple", "banana", "grapes", "mango", "orange"];
+/**
+ * Filter array items based on search criteria (query)
+ */
+function filterItems(arr, query) {
+  return arr.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+}
+console.log("Includes 'ap' : ",filterItems(fruits, "ap")); // ['apple', 'grapes']
+console.log("Includes 'an' : ",filterItems(fruits, "an")); // ['banana', 'mango', 'orange']
+
+
+//Using filter() on sparse arrays 
+//filter will skip empty slots
+console.log([1,2,3,5,  , undefined].filter((x) => x === undefined)); // [undefined]
+console.log([1, ,2, ,4, undefined].filter((x) => x !== 2)); // [1, 4, undefined]
+
+
+
+//Affecting Initial Array (modifying, appending and deleting)
+// Modifying each word
+let words = ["spray", "limit", "exuberant", "destruction", "elite", "present"];
+
+const modifiedWords = words.filter((word, index, arr) => {
+  arr[index + 1] += " extra";
+  return word.length < 16;
+});
+
+console.log(modifiedWords);
+// Notice there are three words below length 6, but since they've been modified one is returned
+// ["spray"]
 
 
 
