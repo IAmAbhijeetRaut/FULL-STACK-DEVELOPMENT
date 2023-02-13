0a8445d36b6829510fdd935460bb0785e7ab6562
abhijeet raut <abhijeet00900@gmail.com>
2023-02-13 11:43:57 +0530

    Rearranging Comments

--- NODEJS/JAVASCRIPT-PART/03-ARRAY_METHODS/2_copyWithin_Method.js
+++ NODEJS/JAVASCRIPT-PART/03-ARRAY_METHODS/2_copyWithin_Method.js
@@ -6,16 +6,7 @@
  3. The copyWithin() method preserves empty slots. If the region to be copied from is sparse, the empty slots' 
     corresponding new indices are deleted and also become empty slots.
 */
-//Syntax
-/* 
-copyWithin(target)
-copyWithin(target, start)
-copyWithin(target, start, end)
 
-target -> index at which to copy the sequence to 
-start ->  index at which to start copying elements from -> if not specified/omitted then 0 is used
-end ->    index at which to end copying elements from -> if omitted then it causes all elements until the end to be copied.
-*/
 
 //Basic Example
 const array_1 = [1,2,3,4,5,6,7,8,9,10];
@@ -28,3 +19,13 @@ console.log("copyWithin-1 for array_1 : ",array_1);
 console.log("copyWithin method on sparse arrays : ", [1, , 3].copyWithin(2, 1, 2)); // [1, empty, empty]
 
 
+//Syntax
+/* 
+copyWithin(target)
+copyWithin(target, start)
+copyWithin(target, start, end)
+
+target -> index at which to copy the sequence to 
+start ->  index at which to start copying elements from -> if not specified/omitted then 0 is used
+end ->    index at which to end copying elements from -> if omitted then it causes all elements until the end to be copied.
+*/
