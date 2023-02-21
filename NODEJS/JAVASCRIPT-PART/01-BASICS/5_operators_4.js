d65210d41d347c6ad90fc20dc51a9b7d9c30f16e
abhijeet raut <abhijeet00900@gmail.com>
2023-02-21 19:04:30 +0530

    Nullish and Exponential Operator

--- NODEJS/JAVASCRIPT-PART/01-BASICS/5_operators_4.js
+++ NODEJS/JAVASCRIPT-PART/01-BASICS/5_operators_4.js
@@ -52,3 +52,46 @@ console.log(result_2); // false
 
 // The following expression uses multiple && operators:
 let result = value1 && value2 && value3;
+
+// Example for Logical OR assignment operator (||=):
+// The logical OR assignment operator (||=) assigns the right operand to the left operand if the left operand is falsy.
+// let title_1;
+// title_1 ||= 'untitled';
+// console.log(title_1); // Output: untitled
+
+// // Another example for Logical OR assignment operator (||=):
+// let title_2 = 'JavaScript Awesome';
+// title_2 ||= 'untitled';
+// console.log(title_2); // Output: 'JavaScript Awesome'
+
+// Example for Logical AND assignment operator (&&=):
+// The logical AND assignment operator (&&=) only assigns y to x if x is truthy.
+// let person = {
+// firstName: 'Jane',
+// lastName: 'Doe',
+// };
+// person.lastName &&= 'Smith';
+// console.log(person); // Output: {firstName: 'Jane', lastName: 'Smith'}
+
+// // Example for nullish coalescing assignment operator (??=):
+// // The nullish coalescing assignment operator (??=) only assigns y to x if x is nullish.
+// let user = {
+// username: 'Satoshi'
+// };
+// user.nickname ??= 'anonymous';
+// console.log(user); // Output: {username: 'Satoshi', nickname:'anonymous'}
+
+//The Above Code for logical OR , logical AND is commented because it is not supported by Node Versions < 15.0, only 15.0 and aboce versions provide support for it.
+
+
+/*
+Key Points:
+- ES2021 introduces three logical assignment operators: Logical OR assignment operator (||=), Logical AND assignment operator (&&=), and Nullish coalescing assignment operator (??=).
+- The logical OR assignment operator (||=) assigns the right operand to the left operand if the left operand is falsy.
+- The logical AND assignment operator (&&=) only assigns y to x if x is truthy.
+- The nullish coalescing assignment operator (??=) only assigns y to x if x is nullish.
+- The logical OR assignment operator (||=) is equivalent to the logical OR operator (||).
+- The logical AND assignment operator (&&=) is equivalent to the logical AND operator (&&).
+- The nullish coalescing assignment operator (??=) is equivalent to the nullish coalescing operator (??).
+- All three logical assignment operators short-circuit, meaning they only perform an assignment when the left operand is falsy for logical OR assignment operator (||=), truthy for logical AND assignment operator (&&=), and nullish for nullish coalescing assignment operator (??=).
+*/
---
+++ NODEJS/JAVASCRIPT-PART/01-BASICS/5_operators_5.js
@@ -0,0 +1,66 @@
+// The nullish coalescing operator (??) accepts two values and returns the second value if the first one is null or undefined.
+// The nullish coalescing operator is equivalent to if(result === null || result === undefined)
+//const result_1 = value1 ?? value2;
+
+// Example 1: Using the nullish coalescing operator to return the second value when the first one is null
+const name_1 = null ?? 'John';
+console.log(name_1); // 'John'
+
+// Example 2: Using the nullish coalescing operator to return the second value when the first one is undefined
+const age = undefined ?? 28;
+console.log(age);//28
+
+// Example 3: Using the logical OR operator (||) to assign a default value to a variable
+// The variable count is undefined and is coerced to false, therefore the result_1 is 1
+let count_1;
+let result_2 = count_1 || 1;
+console.log(result_2); // 1
+
+// Example 4: Using the logical OR operator (||) to assign a default value to a variable
+// The variable count is 0, which is coerced to false, therefore the result is 1
+let count = 0;
+let result_3 = count || 1;
+console.log(result_3); // 1
+
+// The nullish coalescing operator is short-circuited and does not evaluate the second value if the first operand is neither undefined nor null
+let result_4 = 1 ?? console.log('Hi');
+// The second expression is not evaluated because the first value is 1, which is not null or undefined
+
+let result_5 = undefined ?? console.log('Hi');
+// The second expression is evaluated because the first value is undefined
+// Output: 'Hi'
+
+// Wrapping the expression on the left of the ?? operator in parentheses explicitly specifies the operator precedence
+const result_6 = (null || undefined) ?? 'OK';
+console.log(result_6); // 'OK'
+
+// This code block demonstrates how to use the exponentiation operator (**) in JavaScript to raise a number to the power of an exponent.
+
+// Using the Math.pow() method:
+let result = Math.pow(2,2);
+console.log(result); // 4
+
+result = Math.pow(2,3);
+console.log(result); // 8
+
+// Using the exponentiation operator (**):
+result = 2 ** 2;
+console.log(result); // 4
+
+result = 2 ** 3;
+console.log(result); // 8
+
+// Using the exponentiation operator (**) with values of the bigint type:
+result = 2n ** 3n;
+console.log(result); // 8n
+
+// Using the exponentiation operator (**) in the infix notation:
+let x = 2;
+x **= 4;
+console.log(x); // 16
+
+// Using parentheses to disambiguate operator precedence:
+result = (-2)**3;
+console.log(result); // -8
+
+// Note that JavaScript does not allow you to put a unary operator immediately before the base number.
